762e9196502685eefe3a62a7bd6a68a25a040c7a
Tadas <tadasgailevicius@yahoo.com>
2021-03-12 14:37:41 +0200

    Add Constants.kt Constants

---
+++ app/src/main/java/com/example/postapp/other/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.postapp.other
+
+object Constants {
+
+    const val DATABASE_NAME = "posts_db"
+
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
+
+    const val BASE_URL = "http://192.168.8.103:8001"
+
+    const val JSONPLACEHOLDER_POSTS = "https://jsonplaceholder.typicode.com/posts"
+    const val JSONPLACEHOLDER_USERS = "https://jsonplaceholder.typicode.com/users"
+    const val USERS_IMAGES = "https://source.unsplash.com/collection/542909/?sig={user_id}"
+
+
+
+
+
+}
