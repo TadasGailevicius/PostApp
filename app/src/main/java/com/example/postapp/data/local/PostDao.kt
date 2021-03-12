bb723b7dbdc34894b5a1833d6d4cb3d0cd43d46e
Tadas <tadasgailevicius@yahoo.com>
2021-03-12 23:46:48 +0200

    Add insertPost function to PostDao.kt

--- app/src/main/java/com/example/postapp/data/local/PostDao.kt
+++ app/src/main/java/com/example/postapp/data/local/PostDao.kt
@@ -1,12 +1,17 @@
 package com.example.postapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.postapp.data.local.entities.Post
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PostDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPost(post: Post)
 
     @Query("SELECT * FROM posts WHERE id = :postID")
     suspend fun getPostById(postID: String): Post?
