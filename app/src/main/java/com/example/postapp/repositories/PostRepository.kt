9691d5d2b5b958289824cf75d83d26be14278950
Tadas <tadasgailevicius@yahoo.com>
2021-03-12 19:37:25 +0200

    Update PostRepository.kt Repository

--- app/src/main/java/com/example/postapp/repositories/PostRepository.kt
+++ app/src/main/java/com/example/postapp/repositories/PostRepository.kt
@@ -1,4 +1,56 @@
 package com.example.postapp.repositories
 
-class PostRepository {
+import android.app.Application
+import com.example.postapp.data.local.PostDao
+import com.example.postapp.data.local.entities.Post
+import com.example.postapp.data.remote.PostApi
+import com.example.postapp.other.Resource
+import com.example.postapp.other.checkForInternetConnection
+import com.example.postapp.other.networkBoundResource
+import kotlinx.coroutines.flow.Flow
+import java.lang.Exception
+import javax.inject.Inject
+
+class PostRepository @Inject constructor(
+    private val postDao: PostDao,
+    private val postApi: PostApi,
+    private val context: Application
+) {
+
+    suspend fun insertPosts(posts: List<Post>) {
+        posts.forEach { insertPost(it) }
+    }
+
+    suspend fun insertPost(post: Post) {
+        val response = try {
+            postApi.addPost(post)
+        } catch (e: Exception) {
+            null
+        }
+
+        suspend fun getPostById(postID: String) = postDao.getPostById(postID)
+
+        fun getAllPosts(): Flow<Resource<List<Post>>> {
+            return networkBoundResource(
+                query = {
+                    postDao.getAllPosts()
+                },
+                fetch = {
+                    postApi.getPosts()
+                },
+
+                saveFetchResult = { response ->
+                    response.body()?.let {
+                        insertPosts(it)
+                    }
+                },
+
+                shouldFetch = {
+                    checkForInternetConnection(context)
+                }
+            )
+        }
+
+
+    }
 }
