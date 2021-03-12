3cd0103e403deccc9e78a67137040af28617d6e3
Tadas <tadasgailevicius@yahoo.com>
2021-03-12 10:57:46 +0200

    Add PostsDatabase.kt

---
+++ app/src/main/java/com/example/postapp/data/local/PostsDatabase.kt
@@ -0,0 +1,17 @@
+package com.example.postapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.postapp.data.local.entities.Post
+
+@Database(
+        entities = [Post::class],
+        version = 1
+)
+
+@TypeConverters(Converters::class)
+abstract class PostsDatabase : RoomDatabase() {
+
+    abstract fun postDao(): PostDao
+}
