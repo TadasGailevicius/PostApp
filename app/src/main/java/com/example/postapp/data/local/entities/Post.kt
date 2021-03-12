ee58c9d7d11912775b88ea9620ad2ddb97a75d08
Tadas <tadasgailevicius@yahoo.com>
2021-03-12 15:52:01 +0200

    Fix Post.kt Entity

--- app/src/main/java/com/example/postapp/data/local/entities/Post.kt
+++ app/src/main/java/com/example/postapp/data/local/entities/Post.kt
@@ -1,12 +1,15 @@
 package com.example.postapp.data.local.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "posts")
 data class Post (
-    val userId: String,
-    val id: String,
-    val title: String,
-    val body: String
+        val userId: String,
+        @PrimaryKey(autoGenerate = false)
+        val id: String = UUID.randomUUID().toString(),
+        val title: String,
+        val body: String
 
 )
