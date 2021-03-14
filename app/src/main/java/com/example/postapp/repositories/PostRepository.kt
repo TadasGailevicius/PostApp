c037762193357599135017590f7b860075d97ebe
Tadas <tadasgailevicius@yahoo.com>
2021-03-14 23:06:11 +0200

    Update PostRepository.kt Repository

--- app/src/main/java/com/example/postapp/repositories/PostRepository.kt
+++ app/src/main/java/com/example/postapp/repositories/PostRepository.kt
@@ -2,11 +2,16 @@ package com.example.postapp.repositories
 
 import android.app.Application
 import com.example.postapp.data.local.PostDao
+import com.example.postapp.data.local.entities.LocallyDeletedPostID
 import com.example.postapp.data.local.entities.Post
+import com.example.postapp.data.remote.JsonplaceholderPostsApi
+import com.example.postapp.data.remote.JsonplaceholderUsersApi
 import com.example.postapp.data.remote.PostApi
+import com.example.postapp.data.remote.requests.DeletePostRequest
 import com.example.postapp.other.Resource
 import com.example.postapp.other.checkForInternetConnection
 import com.example.postapp.other.networkBoundResource
+import retrofit2.Response
 
 import kotlinx.coroutines.flow.Flow
 import java.lang.Exception
@@ -15,11 +20,15 @@ import javax.inject.Inject
 class PostRepository @Inject constructor(
     private val postDao: PostDao,
     private val postApi: PostApi,
+    private val jsonplaceholderPostsApi: JsonplaceholderPostsApi,
+    private val jsonplaceholderUsersApi: JsonplaceholderUsersApi,
     private val context: Application
 ) {
-    suspend fun insertPosts(posts: List<Post>) {
-        posts.forEach { insertPost(it) }
-    }
+    suspend fun getJPPosts() =
+        jsonplaceholderPostsApi.getJPPosts()
+
+    suspend fun getJPUsers() =
+        jsonplaceholderUsersApi.getJPUsers()
 
     suspend fun insertPost(post: Post) {
         val response = try {
@@ -28,16 +37,54 @@ class PostRepository @Inject constructor(
             null
         }
 
-        if (response != null && response.isSuccessful) {
+        if(response != null && response.isSuccessful) {
             postDao.insertPost(post.apply { isSynced = true })
         } else {
             postDao.insertPost(post)
         }
     }
 
+    suspend fun insertPosts(posts: List<Post>) {
+        posts.forEach { insertPost(it) }
+    }
+
+    suspend fun deletePost(postID: String) {
+        val response = try {
+            postApi.deletePost(DeletePostRequest(postID))
+        } catch (e: Exception) {
+            null
+        }
+        postDao.deletePostById(postID)
+        if(response == null || !response.isSuccessful) {
+            postDao.insertLocallyDeletedPostID(LocallyDeletedPostID(postID))
+        } else {
+            deleteLocallyDeletedPostID(postID)
+        }
+    }
+
     fun observePostByID(postID: String) = postDao.observePostById(postID)
 
+    suspend fun deleteLocallyDeletedPostID(deletedPostID: String) {
+        postDao.deleteLocallyDeletedPostID(deletedPostID)
+    }
+
     suspend fun getPostById(postID: String) = postDao.getPostById(postID)
+
+    private var curPostsResponse: Response<List<Post>>? = null
+
+    suspend fun syncPosts() {
+        val locallyDeletedPostIDs = postDao.getAllLocallyDeletedPostIDs()
+        locallyDeletedPostIDs.forEach { id -> deletePost(id.deletedPostID) }
+
+        val unsyncedPosts = postDao.getAllUnsyncedPosts()
+        unsyncedPosts.forEach { post -> insertPost(post) }
+
+        curPostsResponse = postApi.getPosts()
+        curPostsResponse?.body()?.let { posts ->
+            postDao.deleteAllPosts()
+            insertPosts(posts.onEach { post -> post.isSynced = true })
+        }
+    }
 
     fun getAllPosts(): Flow<Resource<List<Post>>> {
         return networkBoundResource(
@@ -45,12 +92,13 @@ class PostRepository @Inject constructor(
                     postDao.getAllPosts()
                 },
                 fetch = {
-                    postApi.getPosts()
+                    syncPosts()
+                    curPostsResponse
                 },
 
                 saveFetchResult = { response ->
-                    response.body()?.let {
-                        insertPosts(it)
+                    response?.body()?.let {
+                        insertPosts(it.onEach { post -> post.isSynced = true })
                     }
                 },
 
