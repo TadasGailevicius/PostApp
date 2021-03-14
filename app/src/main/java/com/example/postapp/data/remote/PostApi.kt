b0cbf81ec00fd3a37f56e6be35c8cd5202d959cc
Tadas <tadasgailevicius@yahoo.com>
2021-03-14 22:58:28 +0200

    Update PostApi.kt Api

--- app/src/main/java/com/example/postapp/data/remote/PostApi.kt
+++ app/src/main/java/com/example/postapp/data/remote/PostApi.kt
@@ -1,6 +1,7 @@
 package com.example.postapp.data.remote
 
 import com.example.postapp.data.local.entities.Post
+import com.example.postapp.data.remote.requests.DeletePostRequest
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
@@ -9,11 +10,16 @@ import retrofit2.http.POST
 
 interface PostApi {
 
-    @GET("/getPosts")
-    suspend fun getPosts(): Response<List<Post>>
-
     @POST("/addPost")
     suspend fun addPost(
-        @Body post: Post
+            @Body post: Post
     ): Response<ResponseBody>
+
+    @POST("/deletePost")
+    suspend fun deletePost(
+            @Body deletePostRequest: DeletePostRequest
+    ): Response<ResponseBody>
+
+    @GET("/getPosts")
+    suspend fun getPosts(): Response<List<Post>>
 }
