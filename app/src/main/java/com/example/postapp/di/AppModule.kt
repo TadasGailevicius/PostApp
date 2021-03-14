492fab786638a11e34d2571d98acd98ae247fc81
Tadas <tadasgailevicius@yahoo.com>
2021-03-14 22:54:27 +0200

    Update AppModule.kt DI

--- app/src/main/java/com/example/postapp/di/AppModule.kt
+++ app/src/main/java/com/example/postapp/di/AppModule.kt
@@ -5,11 +5,18 @@ import android.content.SharedPreferences
 import androidx.room.Room
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.postapp.R
 import com.example.postapp.data.local.PostsDatabase
+import com.example.postapp.data.remote.JsonplaceholderPostsApi
+import com.example.postapp.data.remote.JsonplaceholderUsersApi
 import com.example.postapp.data.remote.PostApi
 import com.example.postapp.other.Constants.BASE_URL
 import com.example.postapp.other.Constants.DATABASE_NAME
 import com.example.postapp.other.Constants.ENCRYPTED_SHARED_PREF_NAME
+import com.example.postapp.other.Constants.JSONPLACEHOLDER
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -49,6 +56,26 @@ object AppModule {
 
     @Singleton
     @Provides
+    fun provideJsonplacholderPostsApi() : JsonplaceholderPostsApi {
+        return Retrofit.Builder()
+            .baseUrl(JSONPLACEHOLDER)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(JsonplaceholderPostsApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideJsonplacholderUsersApi() : JsonplaceholderUsersApi {
+        return Retrofit.Builder()
+            .baseUrl(JSONPLACEHOLDER)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(JsonplaceholderUsersApi::class.java)
+    }
+
+    @Singleton
+    @Provides
     fun provideEncryptedSharedPreferences(
             @ApplicationContext context: Context
     ): SharedPreferences {
@@ -63,4 +90,15 @@ object AppModule {
                 EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
     }
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+            @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+            RequestOptions()
+                    .placeholder(R.drawable.img_avatar)
+                    .error(R.drawable.img_avatar)
+                    .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
 }
