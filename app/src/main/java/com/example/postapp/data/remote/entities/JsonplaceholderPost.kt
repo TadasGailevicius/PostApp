6c9fef58fcb67b50cfb9fc81cdd653062a9b3985
Tadas <tadasgailevicius@yahoo.com>
2021-03-17 23:59:23 +0200

    Fix JsonplaceholderPost.kt

--- app/src/main/java/com/example/postapp/data/remote/entities/JsonplaceholderPost.kt
+++ app/src/main/java/com/example/postapp/data/remote/entities/JsonplaceholderPost.kt
@@ -1,8 +1,12 @@
 package com.example.postapp.data.remote.entities
 
+import com.google.gson.annotations.Expose
+
 data class JsonplaceholderPost(
     val body: String,
     val id: Int,
     val title: String,
-    val userId: Int
+    val userId: Int,
+    @Expose(deserialize = false, serialize = false)
+    var isSynced: Boolean = false,
 )
